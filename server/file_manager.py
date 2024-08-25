012c8ea681ea01cde4a243720e43f8472ab4aba3
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-08-25 12:55:08 +0530

    📁file handlers and errors

--- server/file_manager.py
+++ server/file_manager.py
@@ -1,16 +1,18 @@
 import os
 import paramiko
 
+
 class FileManager(paramiko.SFTPServerInterface):
     def __init__(self, root="/tmp/sftp"):
         self.root = root
 
     def _realpath(self, path):
-        return os.path.join(self.root, path.lstrip("/"))
+        return os.path.join(self.root, path.lstrip("/")) # converting path to absolute path
 
     def list_folder(self, path):
-        path = self._realpath(path)
+        path = self._realpath(path) #listing contents
         try:
+            # returning SFTPAttributes for each item in the directory
             return [paramiko.SFTPAttributes.from_stat(os.stat(os.path.join(path, f))) for f in os.listdir(path)]
         except OSError:
             return []
@@ -25,6 +27,7 @@ class FileManager(paramiko.SFTPServerInterface):
     def lstat(self, path):
         path = self._realpath(path)
         try:
+            # Returning SFTPAttributes using lstat
             return paramiko.SFTPAttributes.from_stat(os.lstat(path))
         except OSError:
             return paramiko.SFTPAttributes()
@@ -32,6 +35,7 @@ class FileManager(paramiko.SFTPServerInterface):
     def open(self, path, flags, attr):
         path = self._realpath(path)
         try:
+             # Ensure the file is opened in binary mode if necessary
             binary_flag = getattr(os, 'O_BINARY', 0)
             flags |= binary_flag
             mode = getattr(attr, 'st_mode', None)
@@ -39,6 +43,7 @@ class FileManager(paramiko.SFTPServerInterface):
                 fd = os.open(path, flags, mode)
             else:
                 fd = os.open(path, flags)
+            # Returning handle to opened file
             return paramiko.SFTPHandle(fd)
         except OSError:
             return None
