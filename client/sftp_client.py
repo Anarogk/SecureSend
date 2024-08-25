5939f7bb5298ab555ec5fb1958b12299ff1c8cab
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-08-25 12:49:49 +0530

    📜 readability changes

--- client/sftp_client.py
+++ client/sftp_client.py
@@ -3,8 +3,10 @@ import os
 from common.encryption import encrypt_file, decrypt_file
 from common.compression import compress_file, decompress_file
 
+# class for handling SFTP operations with optional encryption and compression
 class SFTPClient:
     def __init__(self, host, port, username, password=None, key_filename=None):
+        # optional Auth parameters
         self.host = host
         self.port = port
         self.username = username
@@ -13,6 +15,7 @@ class SFTPClient:
         self.client = None
 
     def connect(self):
+        # Create an SSH client and connect server
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.client.connect(self.host, self.port, self.username, self.password, key_filename=self.key_filename)
@@ -22,7 +25,7 @@ class SFTPClient:
             self.client.close()
 
     def upload_file(self, local_path, remote_path, encrypt=False, compress=False):
-        if encrypt:
+        if encrypt: # encrypt and compress
             local_path = encrypt_file(local_path)
         if compress:
             local_path = compress_file(local_path)
@@ -32,19 +35,19 @@ class SFTPClient:
         sftp.close()
 
         if encrypt or compress:
-            os.remove(local_path)  # Remove temporary encrypted/compressed file
+            os.remove(local_path)  # Removed temporary encrypted/compressed file
 
     def download_file(self, remote_path, local_path, decrypt=False, decompress=False):
         sftp = self.client.open_sftp()
         sftp.get(remote_path, local_path, callback=self._progress)
         sftp.close()
 
+        # decrypt and decompress
         if decrypt:
             local_path = decrypt_file(local_path)
         if decompress:
             local_path = decompress_file(local_path)
 
     def _progress(self, transferred, total):
+        # Trasfer progress
         print(f"Transfer progress: {transferred}/{total} bytes")
-
-    # Implement other SFTP operations (list_files, create_directory, delete_file, etc.)
